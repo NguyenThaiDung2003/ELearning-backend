d22c6b65ba519d5974fd73aeb4c3dea7a4141a15
Nguyen Thai Dung <dunghmqd@gmail.com>
2025-04-14 16:38:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "http://localhost:5173",  // Đảm bảo rằng frontend domain được chấp nhận
+  origin:  '*',// Thay thế bằng địa chỉ frontend của bạn nếu cần
   methods: "GET, POST",
   credentials: true,  // Cho phép gửi cookie
 };
